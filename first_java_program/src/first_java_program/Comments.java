2727e43b9e8981824c084079c4f88db95b25fb84
김구하 <toto170@naver.com>
2025-01-16 09:59:52 +0900

    공유 설정 및 최초 업로드

---
+++ first_java_program/src/first_java_program/Comments.java
@@ -0,0 +1,25 @@
+package first_java_program;
+
+/**
+ * 주석을 설명하기 위한 실행 가능한 파일 입니다. (Comments 위에 마우스 올리면 설명나옴)
+ * 
+ * @author 김구하
+ */
+
+public class Comments {
+	public static void main(String[] args) {
+		System.out.println("하이");
+		System.out.println("자바");
+		System.out.println("이즈 굿ㅋ");
+		//주석 이즈 릴리 굿;
+		// 주석? 코드에 대한 설명을 기록하는 방법...
+	chillGuy();
+	}
+	/**
+	 * 
+	 * @param 와ㅇㅇㅇ
+	 */
+	private static void chillGuy() {
+		System.out.println("Chill Guy~~");
+	}
+	}
